08cfd95bf82d706fae8fa1fa704e5aad56346bcb
Mike8790 <michael.lonley87@gmail.com>
2020-06-04 16:25:12 +0100

    call_matlab_function.sh

--- call_matlab_function.sh
+++ call_matlab_function.sh
@@ -4,6 +4,6 @@ subject_number=$1
 echo $subject_number
 
 ### in this case the script folder is same as pwd but in other occasions change the path to your script directory
-script_folder=/MRIWork/MRI-Scratch/SPM_Cluster_Template
+script_folder=/MRIWork/MRIWork06/nr/michael_longley/40_sub_run/analysis_scripts
 cd $script_folder
-matlab -nodisplay -nosplash -nodesktop -r "Cluster_Template('$subject_number');exit;"
+matlab -nodisplay -nosplash -nodesktop -r "cluster_vdm_segment_batch.m('$subject_number');exit;"
